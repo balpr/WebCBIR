9ae4ac812ff518f37022cb3213f6c6b3cc98844d
Achmad Iqbal Priranda <79825994+balpr@users.noreply.github.com>
2021-11-29 16:08:29 +0700

    Update runner.py

--- runner.py
+++ runner.py
@@ -98,7 +98,6 @@ def other_page(page_name):
                              % page_name, 404)
 	return response
 
-if __name__ == '__main__':
-	from waitress import serve
-	serve(app, host="0.0.0.0", port=8080)
+def create_app():
+	return app
 	
